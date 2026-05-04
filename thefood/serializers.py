c3a9208d4533ab7d715811019c437b36e5fe2f2b
ReliefHommy <reliefthai@gmail.com>
2026-05-04 15:44:07 +0200

    add subtitle to field

--- thefood/serializers.py
+++ thefood/serializers.py
@@ -5,7 +5,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         #fields = '__all__'
-        fields = ['id', 'title', 'slug','category','description', 'price', 'stock_quantity', 'image', 'partner_store', 'created_at']
+        fields = ['id', 'title', 'subtitle','slug','category','description', 'price', 'stock_quantity', 'image', 'partner_store', 'created_at']
         read_only_fields = ['id', 'partner_store', 'created_at', 'slug']
 
         extra_kwargs = {
